15e14704c96522fcc281e6621118ee6062cfffdd
vungho <hoquocvung@gmail.com>
2017-11-06 00:32:52 +0700

    [New] Create new user api

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,26 @@
 const express = require('express');
 var fs = require('fs');
 const router = express.Router();
+const userService = require('../modules/dynamoDB/UserServices');
 
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
     res.json({'mess': 'welcome api'});
+});
+
+router.post('/register', (req, res) => {
+    let user = req.data;
+    userService.isExistedUser(user.userName, user.openIdType, function (data){
+        if (data === true){
+            res.json({result: {error: 'User is exit'}});
+        }else{
+            userService.createNewUser(user, function (err, data) {
+                if (err){
+                    res.json({result: {error: 'Fail'}});
+                }
+                res.json(data);
+            });
+        }
+    })
 });
 
 module.exports = router;
